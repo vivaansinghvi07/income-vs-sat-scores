fbc5e7430249ee06fbaee2335504893f8c36466a
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-14 00:02:26 -0400

    program that deals with census data

---
+++ census.py
@@ -0,0 +1,45 @@
+import csv
+
+def dictHeader(code):
+    return f"ZCTA5 {code}!!Households!!Estimate"
+
+def getCensusData(zipCodes):
+
+    # creates output
+    output = {}
+
+    # saves index
+    index = -1
+
+    # gets the index of the reader where the median income is
+    with open("census-data.csv", "r") as f:
+
+        # creates a reader
+        reader = csv.DictReader(f)
+
+        # the name of the field of the first element (the label) 
+        label = reader.fieldnames[0]
+        for i in range(len(reader)):
+            if reader[i][label] == "Median income (dollars)":
+                index = i
+    
+    # returns an array of incomes corresponding to the zip codes entered
+    with open("census-data.csv", "r") as f:
+        
+        # creates a reader in list form so it can be indexed over
+        reader = list(csv.DictReader(f))
+
+        # goes through every zip code given
+        for code in zipCodes:
+
+            # gets the income of the code
+            output[code] = reader[index][dictHeader(code)]
+
+    # returns a dictionary with zip code and corresponding household incomes
+    return output
+
+
+
+        
+
+        
